f680f41eccb5c4e6a76983bed5fa6eda44a24bc6
Kamryn Smith <programersign@gmail.com>
2023-08-07 11:21:44 -0700

    assignment complete

--- server/app.py
+++ server/app.py
@@ -14,13 +14,73 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=["GET", "POST"])
 def messages():
-    return ''
 
-@app.route('/messages/<int:id>')
+    if request.method == "GET":
+        messages = Message.query.order_by("created_at").all()
+        messages_serialized = [message.to_dict() for message in messages]
+
+        response = make_response(
+            jsonify(messages_serialized),
+            200
+        )
+
+        return response
+
+    elif request.method == "POST":
+        message_data = request.get_json()
+
+        new_message = Message(
+            body=message_data["body"],
+            username=message_data["username"]
+        )
+
+        db.session.add(new_message)
+        db.session.commit()
+
+        new_message_serialized = new_message.to_dict()
+
+        response = make_response(
+            jsonify(new_message_serialized),
+            201
+        )
+
+        return response
+
+@app.route('/messages/<int:id>', methods=["PATCH", "DELETE"])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
+
+    if request.method == "PATCH":
+        message_data = request.get_json()
+
+        for attr in message_data:
+            setattr(message, attr, message_data[attr])
+
+        db.session.add(message)
+        db.session.commit()
+
+        message_serialized = message.to_dict()
+
+        response = make_response(
+            jsonify(message_serialized),
+            200
+        )
+
+        return response
+
+    elif request.method == "DELETE":
+        db.session.delete(message)
+        db.session.commit()
+
+        response_body = {
+            "delete_successful": True,
+            "status_code": 200
+        }
+
+        return response_body
+
 
 if __name__ == '__main__':
     app.run(port=5555)
